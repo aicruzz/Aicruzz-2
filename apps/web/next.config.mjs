ec87a0a1f63ed250d6bb4cccd62e8041a091aaa1
Favour Mayowa <techiedevmayowa@gmail.com>
2026-05-28 17:52:46 +0100

    fix vercel standalone build

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,15 +1,23 @@
 // next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // VERY IMPORTANT for Vercel monorepo deployment
+  output: 'standalone',
+
   images: {
     remotePatterns: [
       { protocol: 'http', hostname: 'localhost' },
       { protocol: 'https', hostname: '*.aicruzz.com' },
     ],
   },
+
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:4000',
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL ??
+      'http://localhost:4000',
   },
 };
 
